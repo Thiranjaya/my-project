72b6cfb00349c1c0eab84b3b9faecada351da51f
Thiranjaya <113119381+Thiranjaya@users.noreply.github.com>
2022-09-10 17:49:39 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,12 @@
+units=int(input())
+charge=0
+if units<=50:
+    charge=units*5
+elif units<=150:
+    charge=250+(units-50)*7
+
+else:
+    charge=250+700+(units-150)*10
+charge=charge*1.20
+print("units comsumed=",units)
+print("charges=",charge)
